38f4035d30c435bcd87321f8e224b0e0686d7315
Tanushree Gupta <tanugupta1004@gmail.com>
2025-02-24 12:45:12 +0530

    Added FAQ section

--- app/components/Questions.js
+++ app/components/Questions.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useEffect} from 'react'
 
 const Questions=({ques,answer})=>{
+    
+    const [view, setview] = useState(false)
+    
 
     useEffect(() => {
         const script = document.createElement("script");
@@ -16,14 +19,15 @@ const Questions=({ques,answer})=>{
       }, []); 
     return(
         <div className='cursor-pointer h-[fit] w-[fit] border-zinc-700 bg-zinc-800 p-3 mb-[16px]  rounded-2xl border-2 hover:shadow-2xl hover:shadow-zinc-600/20'>
-            <div className='flex items-center justify-between '>
+            <div className='flex items-center justify-between ' onClick={()=>{setview(!view)}}>
                 <h4 className='text-xl text-white font-semibold' >{ques}</h4>
-                <i className="fa-solid fa-caret-down text-white mr-[16px]"></i>
+                <i className="fa-solid fa-caret-down text-white mr-[16px]  "></i>
+                
             </div>
             
-            <div className='hidden'>
+            { view && <div className='hidden'>
                 <p className='text-sm text-gray-300' >{answer}</p>
-            </div>
+            </div>}
         </div>
     )
 }
