5be2180127b45a80c237132f05467b1b932cc1bc
mannat244 <mannateducation@gmail.com>
2025-02-24 12:27:46 +0530

    abut fixed 2

--- app/components/Questions.js
+++ app/components/Questions.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useEffect} from 'react'
 
 const Questions=({ques,answer})=>{
-    
+
     useEffect(() => {
         const script = document.createElement("script");
         script.src = "https://kit.fontawesome.com/04fddebb2c.js";
@@ -15,12 +15,13 @@ const Questions=({ques,answer})=>{
         };
       }, []); 
     return(
-        <div className='cursor-pointer h-[fit] w-[fit] border-zinc-700 bg-zinc-800 p-3 mb-[16px] border-2 rounded-2xl border-2 hover:shadow-2xl hover:shadow-zinc-600/20'>
+        <div className='cursor-pointer h-[fit] w-[fit] border-zinc-700 bg-zinc-800 p-3 mb-[16px]  rounded-2xl border-2 hover:shadow-2xl hover:shadow-zinc-600/20'>
             <div className='flex items-center justify-between '>
                 <h4 className='text-xl text-white font-semibold' >{ques}</h4>
-                <i class="fa-solid fa-caret-down text-white mr-[16px]"></i>
+                <i className="fa-solid fa-caret-down text-white mr-[16px]"></i>
             </div>
-            <div className=''>
+            
+            <div className='hidden'>
                 <p className='text-sm text-gray-300' >{answer}</p>
             </div>
         </div>
